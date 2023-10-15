02ad697899f5f46784c09225f4f5fbe4fe70048d
aarrddan <aarrddan@gmail.com>
2023-10-15 17:01:47 +0900

    내설정 화면 추가

--- lib/screen/setting_screen.dart
+++ lib/screen/setting_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/screen/login_screen.dart';
 
 class SettingScreen extends StatelessWidget {
   const SettingScreen({super.key});
@@ -7,27 +8,91 @@ class SettingScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('매물 목록'),
-        leading: null, // No back button
+        title: const Text('내 정보'),
+        automaticallyImplyLeading: false,
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                // Navigator.of(context).push(
-                //     MaterialPageRoute(builder: (_) => SellItemScreen()),
-                // );
-              },
-              child: Text(
-                'SettingScreen'
-              ))
-          ],
-        ),
-      ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(15),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Padding(padding: EdgeInsets.only(top: 10)),
+                Text(
+                  '닉네임',
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+                const Padding(padding: EdgeInsets.only(top: 10)),
+                Text(
+                  'email@hansol.com',
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+                const Padding(padding: EdgeInsets.only(top: 10)),
+                TextButton(
+                  child: const Text(
+                    '구글 연결 해제',
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Color(0xff787878),
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => LoginScreen()
+                      )
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: 1,
+            color: Colors.grey,
+          ),
+          const Padding(padding: EdgeInsets.only(top: 10)),
+          ListTile(
+            title: const Text('판매목록'),
+            leading: const Icon(Icons.sell),
+            onTap: () {
+              //
+            },
+          ),
+          ListTile(
+            title: const Text('구매목록'),
+            leading: const Icon(Icons.shopping_cart),
+            onTap: () {
+              //
+            },
+          ),
+          const Padding(padding: EdgeInsets.only(top: 10)),
+          Container(
+            height: 1,
+            color: Colors.grey,
+          ),
+          const Padding(padding: EdgeInsets.only(top: 10)),
+          ListTile(
+            title: const Text('사업장 변경'),
+            leading: const Icon(Icons.place_outlined),
+            onTap: () {
+              //
+            },
+          ),
+          ListTile (
+            title: const Text('FAQ'),
+            leading: const Icon(Icons.help),
+            onTap: () {
+              //
+            },
+          ),
+        ],
+      )
     );
   }
 }
